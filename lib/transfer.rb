2c9993bc9598573787ef7f7a44deeddc23395b85
rmiri <re_miriuk@hotmail.com>
2019-12-12 14:35:55 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
+require "pry"
+
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender,receiver, amount)
+    @sender = sender #sender = Bank.new("sender") HOW DO IT KNOW THAT????
+    @receiver = receiver
+    @amount =  amount
+    @status = "pending"
+  end
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+  def execute_transaction
+      if @status == "pending" && self.valid? && @sender.balance > @amount
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+      else
+        @status = "rejected"
+      end
+      "Transaction rejected. Please check your account balance."
+    end
+  def reverse_transfer
+    if status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+    end
+    @status = "reversed"
+  end
+  # binding.pry
+  # 0
 end
