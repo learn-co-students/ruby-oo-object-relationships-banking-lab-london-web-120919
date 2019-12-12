6b02d15287ac0a878970e1932b741bc875d69597
Mariola Pasko <mariolapasko@Mariolas-MacBook-Air.local>
2019-12-12 16:36:29 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
 
+    attr_reader :name, :balance, :status
+    attr_writer :status, :balance
+
+    def initialize(name) 
+        @name = name 
+        @balance = 1000
+        @status = "open"
+    end 
+
+    def deposit(deposit)
+        @balance += deposit
+    end 
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end 
+
+    def valid?
+        @status == "open" && @balance > 0 ? true : false 
+    end 
+
+    def close_account
+        @status = "closed"
+    end 
+
 end
